122bab01f31e1f92b698dfc6bebf9f8dbbb3f44a
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-05-04 10:10:28 +0100

    Making function names sane again

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ module.exports = function forwardEmitter(src, dest, filterFn) {
     filterFn = function() { return true; };
   }
 
-  function _7b589d15_ffae_4559_b3e4_9c913446711b_newListener(eventName, listener) {
-    if (filterFn(eventName) && listener.name.indexOf('_7b589d15_ffae_4559_b3e4_9c913446711b_') !== 0) src.on(eventName, listener);
+  function __forward_emitter_newListener(eventName, listener) {
+    if (filterFn(eventName) && listener.name.indexOf('__forward_emitter_') !== 0) src.on(eventName, listener);
   }
 
-  function _7b589d15_ffae_4559_b3e4_9c913446711b_removeListener(eventName, listener) {
+  function __forward_emitter_removeListener(eventName, listener) {
     src.removeListener(eventName, listener);
   }
 
   // Listeners bound to the destination emitter should be bound to the source emitter.
-  dest.on('newListener', _7b589d15_ffae_4559_b3e4_9c913446711b_newListener);
+  dest.on('newListener', __forward_emitter_newListener);
 
   // When a listener is removed from the destination emitter, remove it from the source emitter
   // (otherwise it will continue to be called).
-  dest.on('removeListener', _7b589d15_ffae_4559_b3e4_9c913446711b_removeListener);
+  dest.on('removeListener', __forward_emitter_removeListener);
 
 };
